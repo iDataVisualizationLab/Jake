d971d7b9cd5f3dcd69ffa50ae37922d8c2ddc51b
JakeTTU <64665273+JakeTTU@users.noreply.github.com>
2021-05-26 17:31:51 -0500

    Add files via upload

---
+++ SoilComposition/js/colors.js
@@ -0,0 +1,7 @@
+let colors5 = ["#4A8FC2", "#A6C09D", "#FAFA7C", "#EC9248", "#D63128"];
+const colorQuantiles = [0.2, 0.4, 0.6, 0.8, 1.0];
+const continuousColorScale = new d3.scaleLinear().domain(colorQuantiles.map(d=>d-colorQuantiles[0])).range(colors5);
+
+function getColor(val){
+    return continuousColorScale(val);
+}
