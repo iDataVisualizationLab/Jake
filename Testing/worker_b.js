4f9380943ee24c4555900f9eb79483bb8ce74d38
JakeTTU <64665273+JakeTTU@users.noreply.github.com>
2022-06-13 13:13:05 -0500

    TESTING

---
+++ Testing/worker_b.js
@@ -0,0 +1,35 @@
+// onmessage = function(e) {
+//     console.log('Worker: Message received from main script');
+//     const result = e.data[0] * e.data[1];
+//     if (isNaN(result)) {
+//         postMessage('Please write two numbers');
+//     } else {
+//         const workerResult = 'Result: ' + result;
+//         console.log('Worker: Posting message back to main script');
+//         postMessage(workerResult);
+//     }
+// }
+
+onmessage = function(e) {
+    // console.log('Worker: Message received from main script');
+    console.log(e)
+
+    let _data = e.data[0]['b']
+    let iterations = e.data[1]
+
+    // let result = {}
+    // // for (let i = 0; i < iterations; i++){
+    //     result[1] = {}
+    // // }
+
+    let failures = 0
+
+    for (let i = 0; i < iterations; i++){
+        Object.keys(_data).forEach(d =>{
+            Math.random() > +_data[d] ? failures+=1 : null
+        })
+
+    }
+    postMessage(failures)
+
+}
